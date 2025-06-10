c92772a7ebfb1c9b2e3b420386bab759d48a6e77
Maria T. Andersen <mteilmann93@gmail.com>
2025-06-10 12:32:36 +0200

    add endpoint /api/v1/employees

---
+++ index.js
@@ -0,0 +1,21 @@
+const Database = require("better-sqlite3");
+const express = require("express");
+
+// load data base
+const db = new Database("Chinook_Sqlite.sqlite");
+
+// create express server
+const app = express();
+const port = 3000;
+
+app.get("/api/v1", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/api/v1/employees", (req, res) => {
+  res.json(db.prepare("SELECT * FROM Employee").all());
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
