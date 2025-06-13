f3166fc978f782d9d3cba5d99bb51bd3b3fe1acc
Maria T. Andersen <mteilmann93@gmail.com>
2025-06-13 12:18:19 +0200

    implement express router objects in the project so each route (e.g. /api/v1/employees) has its own router in a separated Employees.js file. Make sure to pass the variable db as a parameter when importing and initializing each router

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-const Database = require("better-sqlite3");
 const express = require("express");
+const Database = require("better-sqlite3");
 
-// load data base
+// Initialize the database
 const db = new Database("Chinook_Sqlite.sqlite");
 
-// create express server
+// Initialize Express app
 const app = express();
 const port = 3000;
 
+// Use JSON middleware
 app.use(express.json());
-// app.use(auth);
+
+// Import and initialize routers with 'db'
+const employeesRouter = require("./routes/employees")(db);
+
+// User routers
+app.use("/api/v1/employees", employeesRouter);
 
 app.get("/api/v1", (req, res) => {
   res.send("Hello World!");
@@ -66,142 +72,6 @@ app.get("/api/v1/artists/:id", (req, res) => {
 
   res.json(db.prepare("SELECT * FROM Artist WHERE ArtistId = ?").get(id));
 });
-
-app.get("/api/v1/employees", auth, (req, res) => {
-  const allowedQueryParams = [
-    "LastName",
-    "FirstName",
-    "Title",
-    "ReportsTo",
-    "Address",
-    "City",
-    "State",
-    "Country",
-  ];
-  const sqlQuery = "SELECT * FROM Employee";
-  const queryParts = [];
-  const placeHolderValues = [];
-  let employees = [];
-
-  for (const qp in req.query) {
-    if (allowedQueryParams.includes(qp)) {
-      queryParts.push(`${qp} = ?`);
-      placeHolderValues.push(req.query[qp]);
-    }
-  }
-
-  if (queryParts.length > 0) {
-    employees = db
-      .prepare(sqlQuery + " WHERE " + queryParts.join(" AND "))
-      .all(placeHolderValues);
-  } else {
-    employees = db.prepare(sqlQuery).all();
-  }
-
-  // if (title) {
-  //   queryParts.push("Title = ?");
-  //   placeHolderValues.push(title);
-  // }
-
-  // if (lastName) {
-  //   queryParts.push("LastName = ?");
-  //   placeHolderValues.push(lastName);
-  // }
-
-  // if (city) {
-  //   queryParts.push("City = ?");
-  //   placeHolderValues.push(city);
-  // }
-
-  // if (title || lastName || city) {
-  //   sqlQuery += " WHERE " + queryParts.join(" AND ");
-  //   console.log(sqlQuery);
-  //   employees = db.prepare(sqlQuery).all(placeHolderValues);
-  // } else {
-  //   employees = db.prepare(sqlQuery).all();
-  // }
-
-  res.json(employees);
-});
-
-app.get("/api/v1/employees/:id", (req, res) => {
-  const id = req.params.id;
-
-  const info = db
-    .prepare("SELECT * FROM Employee WHERE EmployeeId = ?")
-    .get(id);
-
-  if (!info) {
-    res.status(404).send("Employee does not exist");
-    return;
-  }
-
-  res.json(db.prepare("SELECT * FROM Employee WHERE EmployeeId = ?").get(id));
-});
-
-app.post("/api/v1/employees", (req, res) => {
-  const firstName = req.body["FirstName"];
-  const lastName = req.body["LastName"];
-
-  const insertStatement = db.prepare(
-    "INSERT INTO Employee (LastName, FirstName) VALUES (?, ?)"
-  );
-
-  const info = insertStatement.run(lastName, firstName);
-  const newEmployeeId = info.lastInsertRowid;
-
-  res.json(
-    db.prepare("SELECT * FROM Employee WHERE EmployeeId = ?").get(newEmployeeId)
-  );
-});
-
-app.put("/api/v1/employees/:id", (req, res) => {
-  const id = req.params.id;
-  const firstName = req.body["FirstName"];
-
-  const updateStatement = db.prepare(
-    "UPDATE Employee SET FirstName = ? WHERE EmployeeId = ?"
-  );
-
-  updateStatement.run(firstName, id);
-
-  res.json(db.prepare("SELECT * FROM Employee WHERE EmployeeId = ?").get(id));
-});
-
-app.delete("/api/v1/employees/:id", (req, res) => {
-  const id = req.params.id;
-  const employeeToBeDeleted = db
-    .prepare("SELECT * FROM Employee WHERE EmployeeId = ?")
-    .get(id);
-
-  const deleteStatement = db.prepare(
-    "DELETE FROM Employee WHERE EmployeeId = ?"
-  );
-
-  deleteStatement.run(id);
-
-  res.json(employeeToBeDeleted);
-});
-
-function auth(req, res, next) {
-  const allowedAPI_KEYS = ["maria"];
-  const key = req.query.API_KEY;
-
-  if (!key) {
-    res.sendStatus(403);
-    return;
-  }
-
-  if (!allowedAPI_KEYS.includes(key)) {
-    res.sendStatus(401);
-    return;
-  }
-
-  if (allowedAPI_KEYS.includes(key)) {
-    next();
-    return;
-  }
-}
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
