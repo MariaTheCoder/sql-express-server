97d1e32750f9575528fa14ef105027b0d15db106
Maria T. Andersen <mteilmann93@gmail.com>
2025-06-17 12:50:47 +0200

    create a route handler that handles access to /register which enables the user to create a  user with just a password and email for now. After that, program a route handler that returns an http code to the user when a user accesses /login

--- routes/Users.js
+++ routes/Users.js
@@ -20,8 +20,33 @@ router.post("/", (req, res) => {
 
   const info = insertStatement.run(email, password, givenName, role);
   res.json(info);
+});
 
-  // res.json(db.prepare("SELECT * FROM User WHERE UserId = ?").get(newestUserId));
+router.post("/register", (req, res) => {
+  const email = req.body["Email"];
+  const password = req.body["Password"];
+
+  const insertStatement = db.prepare(
+    "INSERT INTO User (Email, Password) VALUES (?, ?)"
+  );
+
+  const info = insertStatement.run(email, password);
+  res.json(info);
+});
+
+let success = true;
+
+router.get("/login", (req, res) => {
+  // login new user. For now, alternate whether the login was successful or not
+  if (!success) {
+    success = !success;
+    res.sendStatus(401);
+    return;
+  } else {
+    success = !success;
+    res.sendStatus(200);
+    return;
+  }
 });
 
 module.exports = router;
