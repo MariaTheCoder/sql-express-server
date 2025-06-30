dfbdf9081c9192dde2765befc7627e5b13f0334d
Maria T. Andersen <mteilmann93@gmail.com>
2025-06-30 10:06:17 +0200

    convert form data into valid json string

--- public/index.js
+++ public/index.js
@@ -6,7 +6,11 @@ submitBtn.addEventListener("click", (event) => {
 
   const formData = new FormData(form);
 
-  for (const [key, value] of formData) {
-    console.log(`${key}: ${value}\n`);
-  }
+  // eventually we want to send this form data to the backend so we need to transform formData into a JavaScript object
+  // Object.fromEntries can help us to that
+  const dataObject = Object.fromEntries(formData);
+
+  // next, convert the JavaScript object to a JSON string
+  const payload = JSON.stringify(dataObject);
+  console.log(payload);
 });
